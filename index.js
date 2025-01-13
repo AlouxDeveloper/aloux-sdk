ee1c09178d7759e0e8acec7e8406779f2d9046c6
lavzca12 <43247233+arturo-aloux@users.noreply.github.com>
2025-01-13 12:40:35 -0600

    export business

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const IAMrouter = require('./lib/router')
-const IAMauth = require('./lib/middleware')
-const awsAloux = require('./lib/controllers/operationsAWS')
-const awsBQ = require('./lib/services/bigQuery')
-const YAML = require('yamljs')
-const path = require('path')
+const IAMrouter = require("./lib/router");
+const IAMauth = require("./lib/middleware");
+const awsAloux = require("./lib/controllers/operationsAWS");
+const awsBQ = require("./lib/services/bigQuery");
+const YAML = require("yamljs");
+const path = require("path");
 
-
-const User = require('./lib/models/User')
-const Functions = require('./lib/models/Functions')
-const Permission = require('./lib/models/Permission')
-const Menu = require('./lib/models/Menu')
+const User = require("./lib/models/User");
+const Functions = require("./lib/models/Functions");
+const Permission = require("./lib/models/Permission");
+const Menu = require("./lib/models/Menu");
+const Business = require("./lib/models/Business");
 
 // swagger
-const swagger_path = path.resolve(__dirname, './lib/swagger.yaml')
-const swagger = YAML.load(swagger_path)
+const swagger_path = path.resolve(__dirname, "./lib/swagger.yaml");
+const swagger = YAML.load(swagger_path);
 
-if (process.env.DEBUG === 'true') {
-    swagger['servers'] = []
-    swagger.servers.push({ url: process.env.SWAGGER_SERVER, description: 'DEV' })
+if (process.env.DEBUG === "true") {
+  swagger["servers"] = [];
+  swagger.servers.push({ url: process.env.SWAGGER_SERVER, description: "DEV" });
 }
-
 
 module.exports = {
-    IAMRouter: IAMrouter,
-    IAMAuth: IAMauth,
-    IAMSwagger: swagger,
+  IAMRouter: IAMrouter,
+  IAMAuth: IAMauth,
+  IAMSwagger: swagger,
 
-    IAMUserModel: User,
-    IAMFunctionsModel: Functions,
-    IAMPermissionModel: Permission,
-    IAMMenuModel: Menu,
+  IAMUserModel: User,
+  IAMUserBusiness: Business,
+  IAMFunctionsModel: Functions,
+  IAMPermissionModel: Permission,
+  IAMMenuModel: Menu,
 
-    AlouxAWS: awsAloux,
-    AlouxBQ: awsBQ
-}
+  AlouxAWS: awsAloux,
+  AlouxBQ: awsBQ,
+};
